64da37795f5b5cc3a6c191bfc156ce20464cd76b
Mohamed Sarfraz <oisci0394@gmail.com>
2025-02-12 23:22:44 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+include 'db_connect.php';
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $result = $conn->query("SELECT * FROM users WHERE email='$email'");
+    $user = $result->fetch_assoc();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        echo "Login successful!";
+    } else {
+        echo "Invalid credentials!";
+    }
+}
+?>
+<form method="post">
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Login</button>
+</form>
